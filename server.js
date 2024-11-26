a948a6d7726c83d53c3df2083e529d3904554f01
Challnet <challnet@mail.ru>
2024-11-26 16:50:48 +0800

    final

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(cors({
     origin: process.env.CLIENT_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
 }));
 
 app.use('/api/v1/users', userRouter);
